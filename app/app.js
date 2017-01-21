575b9a6631f106cd535e45a5f04cda11fc9f0dd2
signalcap <slayerpost@gmail.com>
2017-01-21 15:01:52 +0300

    update settings

---
+++ app/app.js
@@ -0,0 +1,10 @@
+'use strict';
+// require("material-design-icons");
+// require('underscore');
+require('normalize.css');
+require('./app.styl');
+
+var __svg__ = { path: 'assets/images/svg/*.svg', name: 'images/svg-build/[hash].buttons.svg' };
+require('webpack-svgstore-plugin/src/helpers/svgxhr')(__svg__);
+
+// console.log('Hello world!');
